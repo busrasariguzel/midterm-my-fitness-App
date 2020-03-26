24099761c43e84e4ce702b686cab23bc850cbc55
busrasariguzel <busra.sariguzel@codeimmersives.com>
2020-03-26 12:23:32 -0400

    added single workout route

--- routes/admin/workoutRoutes.js
+++ routes/admin/workoutRoutes.js
@@ -20,18 +20,27 @@ router.post('/workout', (req,res,next)=>{
     newWorkout.duration = '30 mins'
     newWorkout.image.picture = faker.image.sports();
     newWorkout.description = faker.lorem.paragraph();
-
+    
     newWorkout.save().then(workout => {
         
-        return res.status(200).json({message: 'Workout Added', workout: workout});
+        // return res.status(200).json({message: 'Workout Added', workout: workout});
+        return res.render('main/track', {workout:workout})
     }).catch(err=>{
         
         return res.status(400).json({message:'error', err})
-
-    
 });
     }
 
 );
+
+
+router.get('/single-workout/:id', (req,res,next)=> {
+    Workout.findById({_id:req.params.id}, (err,workout)=>{
+        if(err) return next(err)
+        // res.render('main/single-workout', {workout})
+        return res.status(200).json({message: 'single workout viewed', workout})
+    })
+})
+
 
 module.exports=router;
