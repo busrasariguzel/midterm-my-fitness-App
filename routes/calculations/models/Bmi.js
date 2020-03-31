9f77318600a0ad811a01308f3690f7895aeeb8d0
busrasariguzel <busra.sariguzel@codeimmersives.com>
2020-03-31 15:45:27 -0400

    added a user id to bmi model

--- routes/calculations/models/Bmi.js
+++ routes/calculations/models/Bmi.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 
 const bmiSchema = new mongoose.Schema({
+    owner:{type:mongoose.Schema.Types.ObjectId, ref:'User'},
     weight:{type:Number, required: true, trim: true},
     height: {type:Number, required: true, trim: true},
 });
